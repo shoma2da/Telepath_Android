584087c3f8e24e5434ebbb7dd74d81028722a1f4
shoma2da <marcos.shoichi.matsuda@gmail.com>
2017-04-05 08:23:38 +0900

    音量設定次第では自動再生

--- app/src/main/java/net/shoma2da/android/telepath2/MyFirebaseMessagingService.kt
+++ app/src/main/java/net/shoma2da/android/telepath2/MyFirebaseMessagingService.kt
@@ -4,8 +4,11 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Vibrator
 import android.support.v4.app.NotificationCompat
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -43,7 +46,17 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         vibrator.vibrate(longArrayOf(0, 200, 200, 200, 300, 100, 100, 100), -1)
 
-
+        //音量がONならここで再生しちゃう
+        val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val ringVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING)
+        val musicVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        if (ringVolume != 0 && musicVolume != 0) {
+            //TODO: 「ピピッ」とか入れないとツラいな...
+            val mediaPlayer = MediaPlayer()
+            mediaPlayer.setDataSource(voiceUrl)
+            mediaPlayer.prepare()
+            mediaPlayer.start()
+        }
     }
 
 }
