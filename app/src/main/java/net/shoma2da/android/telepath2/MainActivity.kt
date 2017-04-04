044b0f22bae170227df982d7e4786caf64865198
shoma2da <marcos.shoichi.matsuda@gmail.com>
2017-04-04 21:41:19 +0900

    プッシュ用Tokenを保存

--- app/src/main/java/net/shoma2da/android/telepath2/MainActivity.kt
+++ app/src/main/java/net/shoma2da/android/telepath2/MainActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
+
+    private val instanceId = FirebaseInstanceId.getInstance()
+    private val database = FirebaseDatabase.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +27,17 @@ class MainActivity : AppCompatActivity() {
         if (name.isEmpty() || phoneNumber.isEmpty()) {
             startActivity(Intent(this, SignInActivity::class.java))
         }
+
+        updateToken(phoneNumber)
+    }
+
+    private fun updateToken(phoneNumber: String) {
+        val reference = database.getReference("users")
+        val token = instanceId.token
+        val changedToken = mapOf("$phoneNumber/token" to token)
+        reference.updateChildren(changedToken)
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit().putString("token", token).apply()
     }
 }
